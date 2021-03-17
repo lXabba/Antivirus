d28d929616efef5c744f0aeed3467a49c37b3e2a
lXabba <lll333ll@mail.ru>
2021-03-17 19:57:51 +0300

    БД

---
+++ Part3_Antivirus/Part3_Antivirus/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+namespace Part3_Antivirus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Console.Read();
+            
+        }
+        static void Test()
+        {
+            SqliteConnection sqliteConnection = new SqliteConnection("Data Source=D:\\3 семестр\\БазыДанных\\SystemBDTest.db");
+            sqliteConnection.Open();
+            var command = sqliteConnection.CreateCommand();
+            command.CommandText =
+                                @"
+                SELECT *
+                FROM Test
+            ";
+
+
+            using (var reader = command.ExecuteReader())
+            {
+                for (int i = 0; reader.Read(); i++)
+                {
+                    Console.WriteLine(reader.GetString(1));
+                }
+            }
+            sqliteConnection.Close();
+          
+        }
+    }
+}
