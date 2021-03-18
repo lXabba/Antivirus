28e801c0913d7e52cbce6c69f0413e1d43403d00
lXabba <lll333ll@mail.ru>
2021-03-18 10:10:26 +0300

    UI

---
+++ UI_Antivirus/UI_Antivirus/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace UI_Antivirus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScanPanelButton = new System.Windows.Forms.Button();
+            this.ScanPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FileButton = new System.Windows.Forms.Button();
+            this.DirButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ScanPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ScanPanelButton
+            // 
+            this.ScanPanelButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ScanPanelButton.Font = new System.Drawing.Font("Aznauri Square", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ScanPanelButton.Location = new System.Drawing.Point(13, 13);
+            this.ScanPanelButton.Name = "ScanPanelButton";
+            this.ScanPanelButton.Size = new System.Drawing.Size(160, 63);
+            this.ScanPanelButton.TabIndex = 0;
+            this.ScanPanelButton.Text = "Сканирование";
+            this.ScanPanelButton.UseVisualStyleBackColor = false;
+            this.ScanPanelButton.Click += new System.EventHandler(this.ScanPanelButton_Click);
+            // 
+            // ScanPanel
+            // 
+            this.ScanPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ScanPanel.Controls.Add(this.panel1);
+            this.ScanPanel.Controls.Add(this.DirButton);
+            this.ScanPanel.Controls.Add(this.FileButton);
+            this.ScanPanel.Controls.Add(this.label1);
+            this.ScanPanel.Location = new System.Drawing.Point(180, 13);
+            this.ScanPanel.Name = "ScanPanel";
+            this.ScanPanel.Size = new System.Drawing.Size(611, 431);
+            this.ScanPanel.TabIndex = 1;
+            this.ScanPanel.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Aznauri Square", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(177, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Что желаете сканировать?";
+            // 
+            // FileButton
+            // 
+            this.FileButton.BackColor = System.Drawing.SystemColors.Window;
+            this.FileButton.Font = new System.Drawing.Font("Aznauri Square", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FileButton.Location = new System.Drawing.Point(90, 63);
+            this.FileButton.Name = "FileButton";
+            this.FileButton.Size = new System.Drawing.Size(176, 57);
+            this.FileButton.TabIndex = 1;
+            this.FileButton.Text = "Файл";
+            this.FileButton.UseVisualStyleBackColor = false;
+            // 
+            // DirButton
+            // 
+            this.DirButton.BackColor = System.Drawing.SystemColors.Window;
+            this.DirButton.Font = new System.Drawing.Font("Aznauri Square", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DirButton.Location = new System.Drawing.Point(348, 63);
+            this.DirButton.Name = "DirButton";
+            this.DirButton.Size = new System.Drawing.Size(176, 57);
+            this.DirButton.TabIndex = 2;
+            this.DirButton.Text = "Директория";
+            this.DirButton.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(611, 299);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.Font = new System.Drawing.Font("Aznauri Square", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Сканирование:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(430, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(20, 84);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(576, 212);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(573, 27);
+            this.panel2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "18.03.21";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "9:55";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(140, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "C:/TraliVali/TraTrali.exe";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(314, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "trojan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UI_Antivirus.Properties.Resources.the_best_free_antivirus_protection_of_2016_r4m7;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(803, 456);
+            this.Controls.Add(this.ScanPanel);
+            this.Controls.Add(this.ScanPanelButton);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Antivirus Protasova";
+            this.ScanPanel.ResumeLayout(false);
+            this.ScanPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ScanPanelButton;
+        private System.Windows.Forms.Panel ScanPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button DirButton;
+        private System.Windows.Forms.Button FileButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ UI_Antivirus/UI_Antivirus/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_Antivirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ScanPanelButton_Click(object sender, EventArgs e)
+        {
+            ScanPanel.Visible = true;
+            ScanPanelButton.BackColor = Color.LightBlue;
+        }
+    }
+}
